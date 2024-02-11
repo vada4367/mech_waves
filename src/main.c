7a38d1e63ecc93f50b285fe0d873d0346acd374e
WagnerW <gidra4367@protonmail.com>
2024-02-11 18:15:40 +0300

    works

--- src/main.c
+++ src/main.c
@@ -1,27 +1,28 @@
 
-//#include <stdio.h>
+// #include <stdio.h>
+#include "headers/env.h"
 #include <math.h>
 #include <unistd.h>
-#include "headers/env.h"
 
-const int w = 160, h = 20;
+const int W = 159, H = 20;
 
-int main(void)
+int
+main (void)
 {
-  Environment string = new_env(w);
+  Environment string = new_env (W);
 
   for (int i = 0; i < string.size; i++)
-    string.env[i].pos = sinf(i * 0.1) * 5.0;
+    string.env[i].pos = sinf (i * 0.1) * 5.0;
 
   for (;;)
     {
-      update_env(&string);
-      print_env(string, h);
-      
-      //for (int i = 20; i < string.size; i++)
-      //  printf("%d: %f -- %f\n", i, string.env[i].pos, string.env[i].vel);
+      update_env (&string);
 
-      usleep(50000);
+      print_env (string, H);
+      // for (int i = 00; i < string.size; i++)
+      //   printf("%d: %f -- %f\n", i, string.env[i].pos, string.env[i].vel);
+
+      usleep (50000);
     }
 
   return 0;
--- src/material_env.c
+++ src/material_env.c
@@ -40,12 +40,12 @@ void
 print_env (Environment string, int max_h)
 {
   for (int i = 0; i < 2 * max_h * string.size; i++)
-    printf("\x1B[%d;%dH ", i / string.size, i % string.size);
+    printf ("\x1B[%d;%dH ", i / string.size, i % string.size);
 
   for (int i = 0; i < string.size; i++)
     {
       int y = (int)string.env[i].pos + max_h;
-      printf("\x1B[%d;%dH#", y, i);
+      printf ("\x1B[%d;%dH#", y, i);
     }
 }
 
@@ -55,5 +55,8 @@ update_env (Environment *string)
   for (int i = 0; i < string->size; i++)
     string->env[i].pos += string->env[i].vel;
   for (int i = 1; i < string->size - 1; i++)
-    string->env[i].vel += ((string->env[i - 1].pos + string->env[i + 1].pos) / 2 - string->env[i].pos) / string->env[i].mass;
+    string->env[i].vel
+        += ((string->env[i - 1].pos + string->env[i + 1].pos) / 2
+            - string->env[i].pos)
+           / string->env[i].mass;
 }
